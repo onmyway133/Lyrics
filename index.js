4b4d7aa9a6d5765780e01f5cb19888d383ed4b0d
Khoa Pham <onmyway133@gmail.com>
2017-06-11 18:19:31 +0200

    Wrap function call in rx

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const MenuBar = require('menubar')
 const AppleScript = require('applescript')
-const Promise = require('promise')
+const Rx = require('rxjs/Rx')
 
 const menubar = MenuBar()
 const separator = '---'
 
 menubar.on('ready', function ready () {
   console.log('hello world')
-  detectCurrentTrack()
+  const observable = detectSpotifyTrack()
+  observable.subscribe(
+    function (x) {
+      console.log(x)
+    },
+    function (err) {
+      console.log('Error: ' + err)
+    },
+    function () {
+      console.log('Completed')
+    }
+  )
 })
 
-function detectCurrentTrack() {
+function detectSpotifyTrack() {
   var script = `
     tell application "Spotify"
       set trackId to id of current track as string
@@ -22,22 +33,24 @@ function detectCurrentTrack() {
       return trackId & "---" & trackName & "---" & artworkUrl & "---" & artistName & "---" & albumName
     end tell
   `
-  const promise = new Promise((resolve, reject) => {
-
-  })
-  AppleScript.execString(script, function(error, response) {
-    if (response) {
-      const parts = response.split('---')
-      const json = {
-        trackId: parts[0],
-        trackName: parts[1],
-        artworkUrl: parts[2],
-        artistName: parts[3],
-        albumName: parts[4]
+  const observable = Rx.Observable.create((observer) => {
+    AppleScript.execString(script, (error, response) => {
+      if (response) {
+        const parts = response.split('---')
+        const json = {
+          trackId: parts[0],
+          trackName: parts[1],
+          artworkUrl: parts[2],
+          artistName: parts[3],
+          albumName: parts[4]
+        }
+        observer.next(json)
+        observer.complete()
+      } else {
+        observer.error(error)
       }
-      console.log(json)
-    } else {
-      console.log(error)
-    }
+    })
   })
+
+  return observable
 }
