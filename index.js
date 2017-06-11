0b29c90e86c7693be1017dbdc947220e62b0a4a3
Khoa Pham <onmyway133@gmail.com>
2017-06-11 18:24:14 +0200

    Move file to detectors

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const MenuBar = require('menubar')
-const AppleScript = require('applescript')
 const Rx = require('rxjs/Rx')
+const TrackDetectors = require('./trackDetectors.js')
+const LyricFetchers = require('./lyricFetchers.js')
 
 const menubar = MenuBar()
 const separator = '---'
 
 menubar.on('ready', function ready () {
   console.log('hello world')
-  const observable = detectSpotifyTrack()
+  const observable = TrackDetectors.detectSpotifyTrack()
   observable.subscribe(
     function (x) {
       console.log(x)
@@ -20,37 +21,3 @@ menubar.on('ready', function ready () {
     }
   )
 })
-
-function detectSpotifyTrack() {
-  var script = `
-    tell application "Spotify"
-      set trackId to id of current track as string
-      set trackName to name of current track as string
-      set artworkUrl to artwork url of current track as string
-      set artistName to artist of current track as string
-      set albumName to album of current track as string
-
-      return trackId & "---" & trackName & "---" & artworkUrl & "---" & artistName & "---" & albumName
-    end tell
-  `
-  const observable = Rx.Observable.create((observer) => {
-    AppleScript.execString(script, (error, response) => {
-      if (response) {
-        const parts = response.split('---')
-        const json = {
-          trackId: parts[0],
-          trackName: parts[1],
-          artworkUrl: parts[2],
-          artistName: parts[3],
-          albumName: parts[4]
-        }
-        observer.next(json)
-        observer.complete()
-      } else {
-        observer.error(error)
-      }
-    })
-  })
-
-  return observable
-}
---
+++ trackDetectors.js
@@ -0,0 +1,40 @@
+const Rx = require('rxjs/Rx')
+const AppleScript = require('applescript')
+
+function detectSpotifyTrack() {
+  var script = `
+    tell application "Spotify"
+      set trackId to id of current track as string
+      set trackName to name of current track as string
+      set artworkUrl to artwork url of current track as string
+      set artistName to artist of current track as string
+      set albumName to album of current track as string
+
+      return trackId & "---" & trackName & "---" & artworkUrl & "---" & artistName & "---" & albumName
+    end tell
+  `
+  const observable = Rx.Observable.create((observer) => {
+    AppleScript.execString(script, (error, response) => {
+      if (response) {
+        const parts = response.split('---')
+        const json = {
+          trackId: parts[0],
+          trackName: parts[1],
+          artworkUrl: parts[2],
+          artistName: parts[3],
+          albumName: parts[4]
+        }
+        observer.next(json)
+        observer.complete()
+      } else {
+        observer.error(error)
+      }
+    })
+  })
+
+  return observable
+}
+
+module.exports = {
+  detectSpotifyTrack
+}
