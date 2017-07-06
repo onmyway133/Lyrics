3002d374b80aed107ff2e817602385b40ff86dee
Khoa Pham <onmyway133@gmail.com>
2017-07-06 11:39:30 +0200

    Add Loading, Content, Error component

--- renderer.js
+++ renderer.js
@@ -3,16 +3,64 @@ const Cheerio = require('cheerio')
 const React = require('react')
 const ReactDOM = require('react-dom')
 
+// Component
 class Application extends React.Component {
+  render() {
+    const style = {
+      width: 'auto',
+      height: 'auto'
+    }
+
+    return React.createElement('div', {style},
+      React.createElement(LoadingComponent, {}),
+      React.createElement(ContentComponent, {}),
+      React.createElement(ErrorComponent, {})
+    )
+  }
+}
+
+class LoadingComponent extends React.Component {
+  render() {
+    return React.createElement('div', {}, 
+      React.createElement('p', {}, 'Loading')
+    )
+  }
+}
+
+class ErrorComponent extends React.Component {
+  render() {
+    return React.createElement('div', {},
+      React.createElement('p', {}, 'Error')
+    )
+  }
+}
+
+class ContentComponent extends React.Component {
   render() {
     const style = {
       backgroundColor: 'blue'
     }
 
-    return React.createElement('div', {style})
+    return React.createElement('div', {style},
+      React.createElement(ContentHeaderComponent, {}),
+      React.createElement(ContentBodyComponent, {})
+    )
   }
 }
 
+class ContentHeaderComponent extends React.Component {
+  render() {
+    return React.createElement('div', {})
+  }
+}
+
+class ContentBodyComponent extends React.Component {
+  render() {
+    return React.createElement('div', {})
+  }
+}
+
+// Reload
 function reload() {
   ReactDOM.render(
     React.createElement(Application, {}),
@@ -20,8 +68,10 @@ function reload() {
   )
 }
 
-// Reload
+// Reload initially
 reload()
+
+// IPC
 
 ipc.on('loading', (event, arg) => {
   
